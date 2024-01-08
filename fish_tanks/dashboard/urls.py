e749f9672b73d0373a7753ebf6f45de943c5e2f6
Borhan Tipu <me@imtipu.me>
2024-01-08 21:50:33 +0600

    Fish Tank update view added

--- fish_tanks/dashboard/urls.py
+++ fish_tanks/dashboard/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('htmx/', include('fish_tanks.dashboard.htmx.urls', namespace='htmx')),
     path('all/', FishTankListView.as_view(), name='fish_tank_list'),
     path('create/', FishTankCreateView.as_view(), name='fish_tank_create'),
-    path('deatil/<int:pk>/', FishTankDetailView.as_view(), name='fish_tank_detail'),
+    path('detail/<int:pk>/', FishTankDetailView.as_view(), name='fish_tank_detail'),
+    path('detail/<int:pk>/update/', FishTankUpdateView.as_view(), name='fish_tank_update'),
 ]
